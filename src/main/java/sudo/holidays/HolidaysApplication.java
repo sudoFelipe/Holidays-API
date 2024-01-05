e4650b16e15ca7f1a57306dbbdd9d7f0e6ef5fd6
sudoFelipe <luis.ribeiro121@outlook.com>
2024-01-04 22:35:18 -0300

    Start Project

---
+++ src/main/java/sudo/holidays/HolidaysApplication.java
@@ -0,0 +1,13 @@
+package sudo.holidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidaysApplication.class, args);
+	}
+
+}
